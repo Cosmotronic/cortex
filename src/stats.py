edcb58e4c8371e394753cecbcf254bd6ec0dcffb
Reid McKenzie <rmckenzie92@gmail.com>
2013-08-15 11:26:20 -0500

    clean the original damage projection fn out of the statistics code

--- src/stats.py
+++ src/stats.py
@@ -2,12 +2,13 @@
 # Authored by Reid "arrdem" McKenzie, 10/09/2013
 # Licenced under the terms of the EPL 1.0
 
+
 def roll_combinatons(n_dice, val):
     if(n_dice == 0):
         return 0
 
-    elif((n_dice == 1)
-         and val > 0
+    elif(n_dice == 1 
+         and val > 0 
          and val < 7):
         return 1
 
@@ -20,30 +21,19 @@ def roll_combinatons(n_dice, val):
 
 
 def odds_of_beating(dice, threshold):
-    threshold = max(threshold,2)
-    return (float(sum([roll_combinatons(dice, i)
-                       for i in range(threshold, 6 * dice + 1)]))
-            / (6.0 ** dice)
+    integ = 0
+    for i in range(threshold, 6 * dice + 1):
+        integ += roll_combinatons(dice, i)
+
+    return float(integ) / (6.0 ** dice)
 
 
 def avg_damage(dice, modifier):
-    dmg = 0
-    combos = 0
-
+    dmg, combos = 0, 0
     for i in range(dice, 6 * dice + 1):
         tmp_c = roll_combinatons(dice, i)
         combos += tmp_c
 
         if(i + modifier > 0):
             dmg += (i + modifier) * tmp_c
-
     return float(dmg) / float(combos)
-
-
-def damage(r1, r2):
-    mat,d1,defence = r1
-    pow,d2,arm = r2
-
-    return ("%f%% to hit, %f average dmg dealt"
-            % (odds_of_beating(d1, (defence - mat)),
-               avg_damage(d2, (pow - arm))))
