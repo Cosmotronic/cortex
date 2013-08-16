a2a44f84773b5aa53eace90397c5c55b86c46cee
Reid McKenzie <rmckenzie92@gmail.com>
2013-08-16 16:32:31 -0500

    Improvement: Make docstrings out of old comments
    Add docstrings for all the dispatch_* functions by refactoring their
    comment headers into docstrings and formatting them to match a typical
    manpage. The hope is that a future `help` command can access these to
    provide a `help load` or `help alias` functionality trivially.

--- src/cortex/repl.py
+++ src/cortex/repl.py
@@ -11,8 +11,21 @@ _dispatch = {}
 
 
 def dispatch_load(line, env={}):
-    # case 1:
-    #    load code! gotta be able to read in datafiles...
+    """Load code! gotta be able to read in datafiles...  
+
+    This function takes responsability for loading models and when
+    they are implemented aliases and armies from long term storage in
+    serialized forms to Cortex's runtime for analysis.
+
+    As this is a refactored block out of cortex/repl, the invocation
+    is simply (line, env={ .. }). As with the other dispatch_*
+    functions this function returns an (env, code) pair, where code is
+    true if this was the correct function and it ran, else false.
+
+    Valid commands are:
+        'load models <filename>'
+
+    """
     if(line[0] == "load"):
         # what are we loading?
         if(line[1] == "models"):
@@ -47,8 +60,22 @@ _dispatch['load'] = dispatch_load
 
 
 def dispatch_alias(line, env={}):
-    # case 2:
-    #    alias code! I want to be able to create aliases...
+    """Alias code! I want to be able to create aliases for models...
+
+    This function takes responsability for 
+
+    As this is a refactored block out of cortex/repl, the invocation
+    is simply (line, env={ .. }). As with the other dispatch_*
+    functions this function returns an (env, code) pair, where code is
+    true if this was the correct function and it ran, else false.
+
+    Valid commands are:
+        'alias model <alias> <model name>'
+
+    Note that no checking is done to prevent the creation of circular
+    aliases!
+
+    """
     if(line[0] == "alias"):
         # what alias type are we creating?
         if(line[1] == "model"):
@@ -63,11 +90,22 @@ _dispatch['alias']=dispatch_alias
 
 
 def dispatch_ls(line, env={}):
-    # case 4:
-    #   inspection code! this code allows a user to inspec the
-    #   list of models and aliases which have been loaded thus far
-    #   and hopefully verify that the commands are behaving as
-    #   intended.
+    """Inspection code! 
+    
+    this code allows a user to inspect the list of models and aliases
+    which have been loaded thus far and hopefully verify that the
+    loading commands are behaving as intended.
+
+    As this is a refactored block out of cortex/repl, the invocation
+    is simply (line, env={ .. }). As with the other dispatch_*
+    functions this function returns an (env, code) pair, where code is
+    true if this was the correct function and it ran, else false.
+
+    Valid commands are:
+        'ls models'
+        'ls aliases'
+
+    """
     if(line[0] == "ls"):
         if(line[1] == "models"):
             print("loaded models:")
@@ -89,6 +127,27 @@ _dispatch['ls']=dispatch_ls
 
 
 def dispatch_reset(line, env={}):
+    """Reset code!
+
+    If something goes horribly wrong, this is how you reset cortex
+    back to normal! This command allways succeeds if it is the
+    intended command, and returns an empty env map thus clobbering any
+    weirdness a user or a rogue config file could perpetrate.
+
+    As this is a refactored block out of cortex/repl, the invocation
+    is simply (line, env={ .. }). As with the other dispatch_*
+    functions this function returns an (env, code) pair, where code is
+    true if this was the correct function and it ran, else false.
+
+    Valid commands are:
+        'reset'
+
+    """
+    # FIXME:
+    #    This code relies on a hard coded "basic env" object which
+    #    should be shared with the code in the cortex core so that
+    #    changes to the minimum state of the env only have to occur in
+    #    one place.
     if(line[0] == "reset"):
         print("Hard resetting models and aliases...")
         return ({'models':{}, 'model aliases':{}}, True)
@@ -99,15 +158,18 @@ _dispatch['reset']=dispatch_reset
 
 
 def dispatch_attack(line,env={}):
-    # case 3:
-    #    attack code! this is what this entire tool was built for...
-    #    grammar is as follows:
-    #    "attack <name> [with ([def|arm|pow|mat|rat] [-|+]<number>)+ end] \
-    #            <name> [with ([def|arm|pow|mat|rat] [-|+]<number>)+ end]"
-    #
-    #    This structure allows for attacks to be computed both against
-    #    basic stats and against modified stats, such as a +5 arm
-    #    effect (Stryker's ult), a shield spell or soforth.
+    """Attack code! 
+
+    This is what this entire tool was built for... It parses an attack
+    description, being one model vs another, then computes and dumps a
+    stats table projecting the success likelyhoods and average
+    outcomes of such an attack.
+
+    Valid commands are:
+        'attack <name> [with ([def|arm|pow|mat|rat] [-|+]<number>)+ end] \
+                <name> [with ([def|arm|pow|mat|rat] [-|+]<number>)+ end]'
+
+    """
     if(line[0] == "attack"):
         a_model = line[1]
         a_with,i = helpers.parse_with(line, 2)
